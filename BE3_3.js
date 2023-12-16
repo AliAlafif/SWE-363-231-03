a53ab810dad29a305a20ca28a704fbd5ad9a5a4f
aliafif <aliafif6001@gmail.com>
2023-12-17 02:12:32 +0300

    BE_3-exercise_3

---
+++ BE3_3.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 4000;
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'../main_page.html'));
+  });
+  app.get('/club_players', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'../club_players.html'));
+  });
+  app.get('/match_schedule', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'../match_schedule.html'));
+  });
+  app.get('/tournament_history', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'../tournament_history.html'));
+  });
+  app.get('/contact_us', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'../contact_us.html'));
+  });
+
+const indexRouter = require('./routes/main_page');
+const clubPlayersRouter = require('./routes/club_players');
+const matchScheduleRouter = require('./routes/match_schedule');
+const tournamentHistoryRouter = require('./routes/tournament_history');
+const contactUsRouter = require('./routes/contact_us');
+
+app.use('/', indexRouter);
+app.use('/club_players', clubPlayersRouter);
+app.use('/match_schedule', matchScheduleRouter);
+app.use('/tournament_history', tournamentHistoryRouter);
+app.use('/contact_us', contactUsRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
